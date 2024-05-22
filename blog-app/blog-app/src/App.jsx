d11c179620cbb14f19292c07bbe8d214801ee784
JulianKrese <julianwk123@outlook.com>
2024-05-22 22:29:58 +0200

    more copying and icloud issue fixing

---
+++ blog-app/blog-app/src/App.jsx
@@ -0,0 +1,15 @@
+import HomePage from "./components/HomePage";
+import CategoriesPage from "./components/CategoriesPage";
+import BlogsPage from "./components/BlogsPage";
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+function App() {
+  return (
+    <div className="App">
+      <CategoriesPage />
+    </div>
+  );
+}
+
+export default App;
