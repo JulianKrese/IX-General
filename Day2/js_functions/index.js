5010ef93626d316ba6563295a1fb48f3e22af304
JulianKrese <julianwk123@outlook.com>
2024-05-22 10:10:26 +0200

    updated display for Fibonacci and triangle Area function

--- Day2/js_functions/index.js
+++ Day2/js_functions/index.js
@@ -1,21 +1,43 @@
-let submitBtn = document.getElementById("submit")
-let cancelBtn = document.getElementById("cancel")
-let buttonText = document.getElementById("buttonClickedText")
+let areaSubmitBtn = document.getElementById("submitArea");
+let areaCancelBtn = document.getElementById("cancelArea");
+let areaText = document.getElementById("areaText");
 
-submitBtn.addEventListener("click", function () {
-    buttonText.textContent = "submit clicked!"
-    setInterval(clear, 3000)
+let fibonacciSubmitBtn = document.getElementById("submitFibonacci");
+let fibonacciCancelBtn = document.getElementById("cancelFibonacci");
+let fibonacciText = document.getElementById("fibonacciText");
+
+areaSubmitBtn.addEventListener("click", function () {
+    let a = parseInt(document.getElementById("sideA").value);
+    let b = parseInt(document.getElementById("sideB").value);
+    let c = parseInt(document.getElementById("sideC").value);
+    let arr = [a, b, c];
+    if (isNaN(a)|| isNaN(a) || isNaN(a)) {
+        areaText.textContent = "Enter all fields to complete the calculation";
+    }
+    else {
+        let area = calculateTriangleAreaHeronsFormula(arr);
+        areaText.textContent = area;
+    }
 })
 
-cancelBtn.addEventListener("click", function () {
-    buttonText.textContent = "cancel clicked!"
-    setInterval(clear, 3000)
+areaCancelBtn.addEventListener("click", function () {
+    document.getElementById("sideA").value = NaN;
+    document.getElementById("sideB").value = NaN;
+    document.getElementById("sideC").value = NaN;
+    areaText.textContent = "no buttons clicked yet"
 })
 
-function clear() {
-    clearInterval()
-    buttonText.textContent = "no buttons clicked yet"
-}
+fibonacciSubmitBtn.addEventListener("click", function() {
+    let fibonacciNumbers = parseInt(document.getElementById("fibonacciRepeatingNumbers").value)
+    logXFibonnaciNumbers(fibonacciNumbers);
+    fibonacciText.textContent = "Done! Check the console";
+})
+
+fibonacciCancelBtn.addEventListener("click", function () {
+    document.getElementById("fibonacciRepeatingNumbers").value = NaN;
+    fibonacciText.textContent = "no buttons clicked yet";
+})
+
 
 
 ///// FUNCTIONS ////
@@ -27,7 +49,7 @@ function clear() {
     PARAMS: arr: an array of numerics; represents the sides of a triangle
     RET: the area of a triangle (numeric)
 */
-function calculateAreaOfTriangleHeronsFormula(arr) {
+function calculateTriangleAreaHeronsFormula(arr) {
     let a = arr[0], b = arr[1], c = arr[2];
     let s = ((a + b + c) / 2);
     let area = Math.sqrt( s * (s-a) * (s-b) * (s-c) );
@@ -40,7 +62,7 @@ function calculateAreaOfTriangleHeronsFormula(arr) {
             Fibonacci sequence. x must be greater than or equal to 2.
     RET: NA
 */
-function logXFibonnaciDigits(x) {
+function logXFibonnaciNumbers(x) {
     let prev = 0;
     let curr = 1;
     console.log(prev);
@@ -53,12 +75,3 @@ function logXFibonnaciDigits(x) {
         x--;
     }
 }
-
-/* function calls */
-
-// calculate the area of a triangle
-let triangle = [5,5,5]
-console.log(calculateAreaOfTriangleHeronsFormula(triangle));
-
-// first 10 digits of Fibonacci
-logXFibonnaciDigits(10);
