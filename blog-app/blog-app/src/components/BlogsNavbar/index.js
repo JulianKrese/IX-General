162b321651efc2e40d24e669e3330bc7414ab5d4
JulianKrese <julianwk123@outlook.com>
2024-05-23 10:35:32 +0200

    fixed homepage categories

--- blog-app/blog-app/src/components/BlogsNavbar/index.js
+++ blog-app/blog-app/src/components/BlogsNavbar/index.js
@@ -4,7 +4,6 @@ export default function Navbar() {
   return (
     <nav className="navbar navbar-expand-lg">
       <div style={{ margin: "0px 5%" }} className="container-fluid">
-        <div className="collapse navbar-collapse" id="navbarNav">
           <ul className="navbar-nav ms-auto">
             <li className="nav-item">
               <a className="nav-link active" aria-current="page" href="src/components/HomePage/">
@@ -27,7 +26,6 @@ export default function Navbar() {
               </a>
             </li>
           </ul>
-        </div>
       </div>
     </nav>
   );
--- blog-app/blog-app/src/components/Categories/index.js
+++ blog-app/blog-app/src/components/Categories/index.js
@@ -2,12 +2,12 @@ import React from "react";
 
 export default function Categories({ blogPost }) {
   return (
-    <div className="flex-wrap">
+    <div className="flex-wrap d-flex">
       {blogPost.categories.map((category, index) => {
         return (
           <p
             key={index}
-            className="category-tag"
+            className="category-tag m-2"
             style={{
               color: category.color,
               backgroundColor: category.color + "33",
